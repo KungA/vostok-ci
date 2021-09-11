7632d0b5aa7b2e2f3dd5a9d8e510656cb4875d95
kungurtsev <kungurtsev@skbkontur.ru>
2021-09-11 11:26:49 +0500

    try to print folders

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import * as core from '@actions/core'
+import * as glob from '@actions/glob'
 
 async function run(): Promise<void> {
   try {
     core.info(`Hello Vostok`)
+
+    const projectsGlobber = await glob.create(['!*.Tests'].join('\n'))
+    const projects = await projectsGlobber.glob()
+    console.log(projects)
+    
+    const testsGlobber = await glob.create(['*.Tests'].join('\n'))
+    const tests = await testsGlobber.glob()
+    console.log(tests)
+    
   } catch (error) {
     core.setFailed(error.message)
   }
