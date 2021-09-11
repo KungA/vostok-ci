29bd9f9feb42a3d240310039815b9f148f553ae8
kungurtsev <kungurtsev@skbkontur.ru>
2021-09-11 12:27:22 +0500

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import * as core from '@actions/core'
 import * as glob from '@actions/glob'
+import * as fs from 'fs'
+const axios = require('axios').default;
 
 async function run(): Promise<void> {
   try {
     core.info(`Hello Vostok`)
 
+    const cementZip = await axios.get("https://github.com/skbkontur/cement/releases/download/v1.0.58/63d70a890a8a69703c066965196021afb7a793c1.zip", { responseType: "arraybuffer" });
+    await fs.promises.writeFile("cement.zip", cementZip.data);
+    
     const projectsGlobber = await glob.create(['*/*.csproj', '!*.Tests/*.csproj'].join('\n'))
     const projects = await projectsGlobber.glob()
     console.log(projects)
