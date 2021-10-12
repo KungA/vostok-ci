cc1a6c8e6e5f8ed4729ab9472ef2b8b887e38378
kungurtsev <kungurtsev@skbkontur.ru>
2021-10-12 12:04:21 +0500

    missing file

---
+++ src/helpers.ts
@@ -0,0 +1,6 @@
+﻿import * as github from "@actions/github";
+import os from "os";
+
+export function getTestsCacheKey() {
+    return `${github.context.repo.owner}.${github.context.repo.repo}-${os.platform()}-${process.env.GITHUB_RUN_ID}-${process.env.GITHUB_RUN_ATTEMPT}`;
+}
